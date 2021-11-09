923ab99d442725fd3f8f78421964bcd05fe1077b
GeorgeMonS <jorge.moncada.sosa@gmail.com>
2021-11-09 12:49:22 -0500

    versión 1.0.0 CRUD loopback

---
+++ src/models/order.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property, belongsTo, hasOne} from '@loopback/repository';
+import {Person} from './person.model';
+import {Product} from './product.model';
+
+@model()
+export class Order extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_product: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cant_product: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  state: number;
+
+  @belongsTo(() => Person, {name: 'person'})
+  id_person: string;
+
+  @property({
+    type: 'string',
+  })
+  personId?: string;
+
+  @hasOne(() => Product)
+  product: Product;
+
+  constructor(data?: Partial<Order>) {
+    super(data);
+  }
+}
+
+export interface OrderRelations {
+  // describe navigational properties here
+}
+
+export type OrderWithRelations = Order & OrderRelations;
---
+++ src/models/person.model.ts
@@ -0,0 +1,61 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Order} from './order.model';
+
+@model()
+export class Person extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  cellphone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @hasMany(() => Order)
+  orders: Order[];
+
+  constructor(data?: Partial<Person>) {
+    super(data);
+  }
+}
+
+export interface PersonRelations {
+  // describe navigational properties here
+}
+
+export type PersonWithRelations = Person & PersonRelations;
